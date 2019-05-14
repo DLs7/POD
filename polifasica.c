86492af4a96d1993f7cc9c102b95b10ba8647883
DLs7 <agasta@inf.ufsm.br>
2019-05-14 14:29:30 -0300

    nova polifasica

--- polifasica.c
+++ polifasica.c
@@ -3,24 +3,87 @@
 #include <string.h>
 #include <time.h>
 
+#define WAYS 3
+#define SIZE 16
+
 void randomFile (char *fileName, int size);
 
 void randomFile (char *fileName, int size) 
 {
-    FILE *file = fopen (fileName, "w");
+    char * aux = (char*)malloc((sizeof(char) * strlen(fileName)) + 5);
+    strcpy(aux, fileName);
+    strcat(aux, ".txt");
+
+    FILE * file = fopen (aux, "w");
 
     for (int i = 0; i<size; i++) {
         int n = rand()%100;
-        fprintf(file, "%d ", n);
+        fprintf(file, "%d\n", n);
     }
-
-    fprintf(file, "%d", -1);
-
-    fclose(file);
-
+    
     return;
 }
 
+void open_temp(char * arquivo, char ** nome_arquivos, FILE ** arquivos, int tam){
+  for (int i = 0; i < (tam + 1); i++){
+    // cria nome do arq temporario e abre
+    sprintf(nome_arquivos[i],"%s%d.txt", arquivo, i);
+    arquivos[i] = fopen(nome_arquivos[i], "w");
+  }
+}
+
+void close_temp(FILE ** arquivos, int tam){
+  for (int i = 0; i < (tam + 1); i++){
+    // cria nome do arq temporario e abre
+    fclose(arquivos[i]);
+  }
+}
+
+int fib(int n) 
+{ 
+    if (n <= 1) 
+        return n; 
+    return fib(n-1) + fib(n-2); 
+}
+
+void get_from_file(char * origem, char ** destino, int caminhos){
+    char * aux = (char*)malloc((sizeof(char) * strlen(origem)) + 5);
+    strcpy(aux, origem);
+    strcat(aux, ".txt");
+    
+    FILE ** output = (FILE**)malloc(sizeof(FILE*) * (caminhos + 1));
+    
+    FILE * input = fopen(aux, "r"); 
+    open_temp(origem, destino, output, caminhos);
+
+
+    int n;
+    for(int i = 0; i < caminhos; i++){
+        
+    }
+
+    fclose(input);
+    close_temp(output, caminhos);
+}
+
+char ** distribution(char * arquivo, int caminhos){
+    char ** arquivos = (char**)malloc(sizeof(char*) * (caminhos + 1));
+
+    for(int i = 0; i < caminhos + 1; i++){
+        arquivos[i] = (char*)malloc(sizeof(char) * strlen(arquivo) + 5);
+    }
+
+    get_from_file(arquivo, arquivos, caminhos);
+    return arquivos;
+}
+
+void polyphase(char * arquivo, int caminhos){
+    char ** arquivos = distribution(arquivo, caminhos);
+}
+
 int main() {
-    randomFile("randomNumbers.txt", 16);
+    srand(time(NULL));
+    char arquivo[] = "randomNumbers";
+    randomFile(arquivo, SIZE);
+    //polyphase(arquivo, WAYS);
 }
