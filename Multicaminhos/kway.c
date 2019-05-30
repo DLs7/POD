bb213c8bfb8cb07916697362ab43d64d0e1c1408
Lucas <lschurer@inf.ufsm.br>
2019-05-30 19:09:40 -0300

    Update kway

--- Multicaminhos/kway.c
+++ Multicaminhos/kway.c
@@ -1,13 +1,366 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "kway.h"
 
 #define RAM 3
 
+// File manipulation functions
+void concatFiles(char* file1Name, char* file2Name) 
+{
+    FILE *file1 = fopen(file1Name, "r+");
+    FILE *file2 = fopen(file2Name, "r");
+
+    // Go to the end of file1.
+    fseek(file1, 0, SEEK_END);
+
+    int n;
+    while (fscanf(file2, "%d", &n) == 1)
+        fprintf(file1, "%d ", n);
+
+    fclose(file1);
+    fclose(file2);
+}
+
+void removeFirstElement(char *fileName)
+{
+    FILE *file = fopen (fileName, "r");
+    FILE *fileAux = fopen ("temp.txt", "w");
+
+    int n;
+    fscanf(file, "%d", &n);
+    while (fscanf(file, "%d", &n) == 1) fprintf(fileAux, "%d ", n);
+
+    fclose(file);
+    fclose(fileAux);
+
+    copyFile(fileName, "temp.txt");
+    remove("temp.txt");
+}
+
+void copyFile(char* file1Name, char* file2Name)
+{
+    FILE *file1 = fopen(file1Name, "w");
+    FILE *file2 = fopen(file2Name, "r");
+
+    int n;
+    while (fscanf(file2, "%d", &n) == 1) 
+        fprintf(file1, "%d ", n);
+
+    fclose(file1);
+    fclose(file2);
+}
+
+void printFile(char* fileName)
+{
+    FILE *file = fopen(fileName, "r");
+    int n;
+    printf("%s - ", fileName);
+    while (fscanf(file, "%d", &n) == 1) 
+        printf("%d ", n);
+    
+    printf("\n");
+}
+
+void createSortedFile(int numberWays, char* fileName)
+{
+    initializeTempWays(numberWays);
+    getNFirsts(numberWays);
+
+    int buffer[RAM];
+    clearBuffer(buffer);
+    initializeBuffer(buffer);
+    
+    FILE *sortedFile = fopen(fileName, "w");
+    fclose(sortedFile);
+
+    int h =0;
+
+    while (1) {
+        if (buffer[0] == -1 && buffer[1] == -1 && buffer[2] == -1) {
+            break;
+        }
+        
+        printBuffer(buffer);
+
+        int lesserIndex = getLesserValue(buffer);
+        FILE *aux = fopen(fileName, "r+");
+        fseek(aux, 0, SEEK_END);
+        fprintf(aux, "%d ", buffer[lesserIndex]);
+        fclose(aux);
+
+        buffer[lesserIndex] = -1;
+
+        char tempWayName[15] = {'t', 'e', 'm', 'p', 'W', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getTempWayName(lesserIndex, tempWayName);
+        FILE *tempWay = fopen(tempWayName, "r");
+        int n;
+        if (fscanf(tempWay, "%d", &n) == 1) {
+            buffer[lesserIndex] = n;
+            removeFirstElement(tempWayName);
+        }
+
+        fclose(tempWay);
+    }
+ 
+    printBuffer(buffer);
+}
+
+// Buffer manipulation functions
+void initializeBuffer(int *buffer)
+{
+    for (int i = 0; i<RAM; i++) {
+        char tempWayName[15] = {'t', 'e', 'm', 'p', 'W', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getTempWayName(i, tempWayName);
+        FILE *tempWay = fopen(tempWayName, "r");
+        int n;
+        if (fscanf(tempWay, "%d", &n) == 1) {
+            buffer[i] = n;
+        }
+
+        removeFirstElement(tempWayName);
+
+        fclose(tempWay);
+    }
+}
+
+void clearBuffer(int* buffer) 
+{
+    for (int i = 0; i<RAM; i++) 
+        buffer[i] = -1;
+}
+
+void sortBuffer (int *buffer)
+{
+    for (int i = 1; i < RAM; i++) {
+        int key = buffer[i];
+        int j = i-1;
+        while ( (j >= 0) && (key < buffer[j]) && key > 0) {
+            buffer[j+1] = buffer[j];
+            j--;
+        }
+        buffer[j+1] = key;  
+    }
+}
+
+void printBuffer(int *buffer)
+{
+    for (int i = 0; i<RAM; i++) {
+        printf("%d ", buffer[i]);
+    }
+
+    printf("\n");
+}
+
+void concatBufferFile(int *buffer, char* fileName) 
+{
+    FILE *file = fopen(fileName, "r+");
+    // Go to the end of file.
+    fseek(file, 0, SEEK_END);
+    for (int i = 0; i<RAM; i++) {
+        // Only values different than -1 are allowed.
+        if (buffer[i] != -1)
+            fprintf(file, "%d ", buffer[i]);
+    }
+
+    fclose(file);
+}
+
+int getLesserValue (int *buffer)
+{
+    int lesser = 0;
+
+    for (int i = 0; i<RAM; i++) {
+        
+        if (buffer[i] != -1) {
+            if (buffer[i] < buffer[lesser])
+        	    lesser = i;
+        } 
+    }
+
+    return lesser;
+}
+
+// Way manipulation functions
+void initializeWays(int numberWays)
+{
+    for (int i = 0; i<numberWays; i++) {
+        char wayName[11] = {'w', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getWayName(i, wayName);
+        FILE *way = fopen(wayName, "w");
+        fclose(way);
+    }
+}
+
+void initializeTempWays(int numberWays)
+{
+    for (int i = 0; i<numberWays; i++) {
+        char tempWayName[15] = {'t', 'e', 'm', 'p', 'W', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getTempWayName(i, tempWayName);
+        FILE *tempWay = fopen(tempWayName, "w");
+        fclose(tempWay);
+    }
+}
+
+void removeTempWays(int numberWays)
+{
+    for (int i = 0; i<numberWays; i++) {
+        char tempWayName[15] = {'t', 'e', 'm', 'p', 'W', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getTempWayName(i, tempWayName);
+        remove(tempWayName);
+    }
+}
+
+static void printWays(int numberWays) 
+{
+    char wayName[11] = {'w', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+    for (int i = 0; i<numberWays; i++) {
+        getWayName(i, wayName);
+        printFile(wayName);
+    }
+}
+
+void createFirstWays(int numberWays, char* fileName)
+{
+    if (numberWays < 1) {
+        printf("Invalid number of ways in fuction createFirstWays. Number of ways must be a positive and bigger than 0 value.\n");
+        return;
+    } 
+
+    FILE *dataFile = fopen(fileName, "r");
+    
+    int buffer[RAM];
+    char wayName[11] = {'w', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+    
+    int n;
+    int i = 0;
+    int aWay = 0;
+    while (fscanf(dataFile, "%d", &n) == 1) {
+        if (i == RAM) {
+            i = 0;
+
+            getWayName(aWay, wayName);
+            sortBuffer(buffer);
+            concatBufferFile(buffer, wayName);
+            clearBuffer(buffer);
+
+            if (aWay == numberWays-1) 
+                aWay = 0;
+            else
+                aWay++;
+
+        }
+        
+        buffer[i] = n;
+    
+        i++;
+    }
+    getWayName(aWay, wayName);
+    sortBuffer(buffer);
+    concatBufferFile(buffer, wayName);
+    clearBuffer(buffer);
 
 
+    fclose(dataFile);
+
+    printf("createFirstWays:\n");
+    printWays(numberWays);
+}
+
+void createWays(int numberWays, char* fileName)
+{
+}
+
+void kWays(int numberWays, char* fileName)
+{
+    initializeWays(numberWays);
+    createFirstWays(numberWays, fileName);
+    createSortedFile(numberWays, "sortedFile.txt");
+}
+
+void getWayName(int index, char* wayName)
+{
+    char aux[4] = {'0', '0', '0', '\0'};
+
+    if (index > 999 || index < 0) {
+        printf("Index out of scope of function getWayName.\n");
+        return;
+    }
+
+    if (index < 9) {
+        aux[2] = index + 48;
+        
+    } else if (index < 99) {
+        aux[1] = ( index / 10 ) + 48;
+        aux[2] = ( index % 10 ) + 48;
+        
+    } else if (index < 999) {
+        aux[0] = ( index / 100 ) + 48;
+        aux[1] = ( ( index / 10 ) % 10 ) + 48;
+        aux[2] = ( index % 10 ) + 48;
+    }
+
+    for (int i = 0; i<3; i++) {
+        wayName[3+i] = aux[i];
+    }
+}
+
+void getTempWayName(int index, char* tempWayName)
+{
+    char aux[4] = {'0', '0', '0', '\0'};
+
+    if (index > 999 || index < 0) {
+        printf("Index out of scope of function getWayName.\n");
+        return;
+    }
+
+    if (index < 9) {
+        aux[2] = index + 48;
+        
+    } else if (index < 99) {
+        aux[1] = ( index / 10 ) + 48;
+        aux[2] = ( index % 10 ) + 48;
+        
+    } else if (index < 999) {
+        aux[0] = ( index / 100 ) + 48;
+        aux[1] = ( ( index / 10 ) % 10 ) + 48;
+        aux[2] = ( index % 10 ) + 48;
+    }
+
+    for (int i = 0; i<3; i++) {
+        tempWayName[7+i] = aux[i];
+    }
+}
+
+void getNFirsts(int numberWays)
+{
+    for (int i = 0; i<numberWays; i++) {
+        char tempWayName[15] = {'t', 'e', 'm', 'p', 'W', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        char wayName[11] = {'w', 'a', 'y', '0', '0', '0', '.', 't', 'x', 't', '\0'};
+        getWayName(i, wayName);
+        getTempWayName(i, tempWayName);
+
+        FILE *way = fopen(wayName, "r");
+        FILE *tempWay = fopen(tempWayName, "w");
+
+        int n;
+        int j = 0;
+        while (fscanf(way, "%d", &n) == 1 && j < RAM) {
+            fprintf(tempWay, "%d ", n);
+            j++;
+        }
+
+        for (int j = 0; j<RAM; j++) {
+            removeFirstElement(wayName);
+        }
+
+        fclose(way);
+        fclose(tempWay);
+    }
+}
 
 void main ()
 {
-    
+    kWays(3, "testFile.txt");
+
 }
--- Multicaminhos/kway.h
+++ Multicaminhos/kway.h
@@ -1,1 +1,141 @@
+// Create k-ways, each one intercalating on the RAM range. Example:
+// 3 ways, RAM = 3
+// 10 12 14 15 18 10 2 34 20 55 87 92 85 17 - fileName.txt
+// First Iteration:
+// *The difference between createFirstWays and createWays is that the first sort the groups of numbers on the RAM range.
+// way000 - 10 12 14 ->(Sort) 10 12 14 
+// way001 - 15 18 10 ->(Sort) 10 15 18 
+// way002 - 2 34 20 ->(Sort) 2 20 34
+// Second Iteration:
+// way000 - 10 12 14 55 87 92
+// way001 - 10 15 18 17 85
+// way002 - 2 20 34
+// Third Iteration:
+// fileName.txt is empty, so all the ways were created, end of the function.
+void createFirstWays(int numberWays, char* fileName);
 void createWays(int numberWays, char* fileName);
+
+
+// Initialize ways and temp ways with empty values.
+void initializeWays(int numberWays);
+void initializeTempWays(int numberWays);
+
+// Delete temp ways.
+void removeTempWays(int numberWays);
+
+// Function to control all the executions needed for the kWays.
+// Step 1: Initialize all the files "wayXXX.txt" with an empty value.
+// Step 2: Call the createFirstWays, passing the fileName as the dataFile.
+// Step 3: Call the createSortedFile.
+// Step 4: Repeat Step 1.
+// Step 4: Call the createWays, passing the file created by sortedFile as the dataFile.
+void kWays(int numberWays, char* fileName);
+
+// Start the process to create a sortedFile.
+// Step 1: Create a k number of tempWayXXX.txt files, initializing it with empty values.
+// Step 2: Each temp way will get the first N values from their respectives ways, where N = RAM. 
+// The values added to tempWayXXX will get removed from the original way.
+// Example:
+// RAM = 3, numberWays = 3;
+// way000.txt - 24 34 45 11 13 35 11 14 15
+// way001.txt - 13 15 43 11 12 32 11
+// way002.txt - 11 11 32 11 13 34 
+// After calling getNFirsts:
+// tempWay000.txt - 24 34 45
+// tempWay001.txt - 13 15 43
+// tempWay002.txt - 11 11 32
+// way000.txt - 11 13 35 11 14 15
+// way001.txt - 11 12 32 11
+// way002.txt - 11 13 34
+// Step 3: Use a buffer to get the first element of each temporary file and then remove it from the tempWayXXX.txt
+// Example:
+// tempWay000.txt - 24 34 45
+// tempWay001.txt - 13 15 43
+// tempWay002.txt - 11 11 32
+// buffer[RAM] - -1 -1 -1
+// After calling the initializeBuffer:
+// tempWay000.txt - 34 45
+// tempWay001.txt - 15 43
+// tempWay002.txt - 11 32
+// buffer[RAM] - 24 13 11
+// Step 4: Open a new file "fileName.txt" to hold the sorted values.
+// Step 5: Call the getLesserValue and store the result on an auxiliary variable called lesserIndex
+// Step 6: Store the buffer[lesserIndex] into the sortedFile.
+// Step 7: Remove the buffer[lesserIndex] from the buffer and add another value gathered from the same temporaryWay.
+// Example:
+// tempWay000.txt - 34 45
+// tempWay001.txt - 15 43
+// tempWay002.txt - 11 32
+// buffer[RAM] - 24 13 11
+// lesserIndex = 2
+// sortedFile.txt - 11
+// buffer[RAM] - 24 13 ?
+// buffer[RAM] - 24 13 11
+// tempWay002.txt - 32
+// Repeat Step 5 until the buffer holds only -1 -1 -1 -1 ... RAM.
+void createSortedFile(int numberWays, char* fileName);
+
+// Concat two files. Example:
+// file1 = 1 2 3
+// file2 = 4 5 6
+// After concatFiles:
+// file1 = 1 2 3 4 5 6
+// file2 = 4 5 6
+void concatFiles(char* file1Name, char* file2Name);
+
+// Remove the first element of a file.
+void removeFirstElement(char *fileName);
+
+// Copy the contents of file2 into file1. Example:
+// file1 = 1 2 3
+// file2 = 4 5 6
+// After copyFile:
+// file1 = 4 5 6
+// file2 = 4 5 6
+void copyFile(char* file1Name, char* file2Name);
+
+// Print the contents of the file on the terminal. Example:
+// fileName = "wayXXX.txt"
+// wayXXX.txt - 1 2 3 4 5 6
+void printFile(char* fileName);
+
+// Returns the index of the lesser value contained inside a buffer.
+int getLesserValue(int *buffer);
+
+// Get the first elements of N tempWays, where N = RAM.
+void initializeBuffer(int* buffer);
+
+// Set all the contents of the buffer to -1.
+void clearBuffer(int* buffer);
+
+// Sort the buffer.
+void sortBuffer(int *buffer);
+
+// Print the contents of the buffer on the terminal.
+void printBuffer(int *buffer);
+
+// Concat the elements of a buffer and a file. Example:
+// fileName - wayXXX.txt
+// buffer[3] - 1 2 3
+// After the function:
+// wayXXX.txt - 1 2 3 
+void concatBufferFile(int *buffer, char* fileName);
+
+// Given a number, returns a string with the follow structure: "wayXXX.txt",
+// where XXX is the index converted to 3 numbers. Example:
+// getWayName(1) = "way001.txt"
+// getWayName(999) = "way999.txt"
+// The maximum number supported by the function is 999 and the minimum is 0.
+void getWayName(int index, char* wayName);
+
+// As getWayName, but it returns a "tempWayXXX.txt"
+void getTempWayName(int index, char* tempWayName);
+
+// Transfer the N first values (where N = RAM) of a wayXXX.txt to its temporary counterpart.
+// Example:
+// RAM = 3;
+// way000.txt = 1 2 3 4 5 6
+// After the function:
+// tempWay000.txt = 1 2 3
+// way000.txt = 4 5 6
+void getNFirsts(int numberWays);
